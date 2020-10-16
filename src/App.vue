48937f0990436d5457f7e6228c2f05974d3ac2f4
zg-zhang <493174665@qq.com>
2020-10-16 16:38:05 +0800

    setup ref 的初使用

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <h2>666</h2>
+    <p>选择</p>
+  </div>
+  <div>
+    <button
+        v-for="(item, index) in list"
+        v-bind:key="index"
+        @click="selectListFun(index)"
+    >
+      {{ index }} : {{ item }}
+    </button>
+  </div>
+  <div>你选择了 {{ selectList }} </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const list = ref(['a', 'b', 'c']);
+    const selectList = ref("");
+    const selectListFun = (index: number) => {
+      selectList.value = list.value[index];
+    }
+    return {
+      list,
+      selectList,
+      selectListFun
+    }
   }
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue' // 引入 vue 文件，并导出 createApp
+import App from './App.vue' // 引入自定义组件
 
-createApp(App).mount('#app')
+createApp(App).mount('#app') // 把 App 挂载到 #app 节点上
